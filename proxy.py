f3849101dc059c0208b8d71483adef7aa87313dd
Wandrille <wandrille.doutte29@yahoo.com>
2025-06-04 19:49:46 +0200

    Ajout headers CORS + fixes images dynamiques

--- proxy.py
+++ proxy.py
@@ -19,18 +19,16 @@ def proxy():
 
     content_type = resp.headers.get('Content-Type', '')
 
-    # HTML : on modifie les liens
     if 'text/html' in content_type:
         soup = BeautifulSoup(resp.text, 'html.parser')
         base_url = target_url
 
-        # Injecter un <base> pour que les scripts / formulaires gardent le bon contexte
-        if not soup.find('base'):
-            base_tag = soup.new_tag('base', href=f"/?url={quote(base_url)}")
-            if soup.head:
-                soup.head.insert(0, base_tag)
+        # Injecter une balise <base> pour maintenir le contexte
+        if not soup.find('base') and soup.head:
+            base_tag = soup.new_tag('base', attrs={'href': f"/?url={quote(base_url)}"})
+            soup.head.insert(0, base_tag)
 
-        # Réécriture des balises principales
+        # Réécrire tous les liens
         for tag in soup.find_all(['a', 'link', 'script', 'img', 'iframe', 'source']):
             for attr in ['href', 'src', 'data-src', 'data-lazy-src', 'srcset']:
                 if tag.has_attr(attr):
@@ -50,12 +48,15 @@ def proxy():
 
         return Response(str(soup), content_type='text/html')
 
-    # Sinon (image, CSS, JS, etc.), on renvoie brut
-    return Response(
-        resp.content,
-        status=resp.status_code,
-        content_type=resp.headers.get('Content-Type', '')
-    )
+    # Sinon (image, CSS, JS, etc.), ajouter les headers CORS
+    headers = {
+        "Content-Type": resp.headers.get("Content-Type", ""),
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET"
+    }
+
+    return Response(resp.content, status=resp.status_code, headers=headers)
 
 if __name__ == '__main__':
     app.run(debug=True)
